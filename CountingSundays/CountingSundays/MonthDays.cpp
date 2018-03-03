e7b362c65025257204d8f7e9c866400f22950ae4
oksmith <ollie.smith131@btinternet.com>
2018-03-03 10:38:15 +0000

    Add files via upload

---
+++ CountingSundays/CountingSundays/MonthDays.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int monthDays(int month, int year)
+{
+	if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12))
+	{
+		return 31;
+	}
+	else if (month == 2)
+	{
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+		{
+			return 29;
+		}
+		else
+		{
+			return 28;
+		}
+	}
+	else
+	{
+		return 30;
+	}
+}
